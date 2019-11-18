f3f37edbeed31664058309c705fd4c07bded0fd6
Justin Chee <justincy.chee@mail.utoronto.ca>
2019-11-18 06:10:12 -0500

    for 18-Nov

--- 08_11_2019.R
+++ 08_11_2019.R
@@ -1,12 +1,16 @@
 # resource: http://bioconductor.org/help/course-materials/2015/BioC2015/bioc2015rnaseq.html
 # resource: https://bioinformatics-core-shared-training.github.io/RNAseq-R/rna-seq-annotation-visualisation.nb.html
 # resource: https://bioconductor.org/packages/release/bioc/vignettes/Rsubread/inst/doc/SubreadUsersGuide.pdf
+# resource: https://www.bioconductor.org/packages/devel/bioc/vignettes/Gviz/inst/doc/Gviz.html#7_composite_plots_for_multiple_chromosomes
 
 library("GenomicFeatures")
 library("Gviz")
 library("GenomicRanges")
 library("GenomicAlignments")
 library("rtracklayer")
+library("Rsubread")
+library("erer")
+library("plyr")
 
 # making txdb
 txdb <- makeTxDbFromGFF("datafiles/Mus_musculus.GRCm38.98.gtf", format="gtf")
@@ -53,6 +57,29 @@ plotTracks(list(gtrack, grtrack, altrack_wt_f), from=25291177, to=25319253, chro
 plotTracks(list(gtrack, grtrack, altrack_wt_m), from=25291177, to=25319253, chromosome="2")
 plotTracks(list(gtrack, grtrack, altrack_mt_f), from=25291177, to=25319253, chromosome="2")
 
+plotTracks(list(gtrack, grtrack, altrack_wt_f, altrack_mt_f, altrack_wt_m), 
+           from=25291177, to=25319253, chromosome="2")
+
+ot <- OverlayTrack(trackList = list(altrack_wt_f, altrack_mt_f, altrack_wt_m))
+
+plotTracks(list(gtrack, grtrack, ot), from=25291177, to=25319253, chromosome="2")
+
+# just coverage
+altrack_wt_f_c <- AlignmentsTrack("datafiles/wildtype_female_Grin1.bam", type=c("coverage"), legend = TRUE)
+altrack_wt_m_c <- AlignmentsTrack("datafiles/wildtype_male_Grin1.bam", type=c("coverage"), legend = TRUE)
+altrack_mt_f_c <- AlignmentsTrack("datafiles/mutant_female_Grin1.bam", type=c("coverage"), legend = TRUE)
+
+displayPars(altrack_wt_f_c) <- list(col = "red", fill = "transparent", legend = TRUE)
+displayPars(altrack_mt_f_c) <- list(col = "blue", fill = "transparent", legend = TRUE)
+displayPars(altrack_wt_m_c) <- list(col = "orange", fill = "transparent", legend = TRUE)
+
+plotTracks(list(gtrack, grtrack, altrack_wt_f_c, altrack_mt_f_c, altrack_wt_m_c), 
+           from=25291177, to=25319253, chromosome="2", legend = TRUE)
+
+ot_c <- OverlayTrack(trackList = list(altrack_wt_f_c, altrack_mt_f_c, altrack_wt_m_c))
+
+plotTracks(list(gtrack, grtrack, ot_c), from=25291177, to=25319253, chromosome="2", legend = TRUE)
+
 # visualize sashimi plot
 altrack_wt_f_s <- AlignmentsTrack("datafiles/wildtype_female_Grin1.bam", type=c("coverage","sashimi"))
 altrack_wt_m_s <- AlignmentsTrack("datafiles/wildtype_male_Grin1.bam", type=c("coverage","sashimi"))
@@ -61,10 +88,46 @@ plotTracks(list(gtrack, grtrack, altrack_wt_f_s), from=25291177, to=25319253, ch
 plotTracks(list(gtrack, grtrack, altrack_wt_m_s), from=25291177, to=25319253, chromosome="2")
 plotTracks(list(gtrack, grtrack, altrack_mt_f_s), from=25291177, to=25319253, chromosome="2")
 
+displayPars(altrack_wt_f_s) <- list(col = "red", fill = "transparent", legend = TRUE)
+displayPars(altrack_mt_f_s) <- list(col = "blue", fill = "transparent", legend = TRUE)
+displayPars(altrack_wt_m_s) <- list(col = "orange", fill = "transparent", legend = TRUE)
+
+ot_s <- OverlayTrack(trackList = list(altrack_wt_f_s, altrack_mt_f_s, altrack_wt_m_s))
+
+plotTracks(list(gtrack, grtrack, ot_s), from=25291177, to=25319253, chromosome="2")
+
 # determining features contained within the gtf file
 gtf <- rtracklayer::import('datafiles/Mus_musculus.GRCm38.98.gtf')
 gtf_df = as.data.frame(gtf)
 feature_list <- unique(gtf_df[7])
+feature_list
+
+# function to find genes with successful alignments
+nonzero_counts <- function(counts, df, cat) {
+  
+  # dataframe to store count data 
+  df <- data.frame(Gene_ID = character(),
+                   exons = integer(), 
+                   genes = integer(), 
+                   introns = integer())
+  
+  for (i in 1:length(counts)) {
+    if (counts[i] != 0) {
+      if (cat == "exons") {
+        df <- rbind(df, data.frame("Gene_ID" = rownames(counts)[i], "exons" = counts[i], 
+                                   "genes" = 0, "introns" = 0))
+      } else if (cat == "genes") {
+        df <- rbind(df, data.frame("Gene_ID" = rownames(counts)[i], "exons" = 0, 
+                                   "genes" = counts[i], "introns" = 0))
+      } else if (cat == "introns") {
+        df <- rbind(df, data.frame("Gene_ID" = rownames(counts)[i], "exons" = 0, 
+                                   "genes" = 0, "introns" = counts[i]))
+      }
+    }
+  }
+  
+  return(df)
+}
 
 # quantifying data with Rsubread
 temp1 <- featureCounts(files="datafiles/wildtype_female_Grin1.bam",
@@ -82,7 +145,7 @@ temp3 <- featureCounts(files="datafiles/mutant_female_Grin1.bam",
 temp4 <- featureCounts(files="datafiles/wildtype_female_Grin1.bam",
                        annot.ext="datafiles/Mus_musculus.GRCm38.98.gtf",
                        isGTFAnnotationFile = TRUE, GTF.featureType="gene", GTF.attrType="gene_id", 
-                       isPairedEnd = TRUE, allowMultiOverlap = TRUE)
+                       isPairedEnd = TRUE, allowMultiOverlap = FALSE)
 temp5 <- featureCounts(files="datafiles/wildtype_male_Grin1.bam",
                        annot.ext="datafiles/Mus_musculus.GRCm38.98.gtf",
                        isGTFAnnotationFile = TRUE, GTF.featureType="gene", GTF.attrType="gene_id", 
@@ -103,3 +166,17 @@ temp9 <- featureCounts(files="datafiles/mutant_female_Grin1.bam",
                        annot.ext="datafiles/Mus_musculus.GRCm38.98.gtf",
                        isGTFAnnotationFile = TRUE, GTF.featureType="CDS", GTF.attrType="gene_id", 
                        isPairedEnd = TRUE)
+
+# comparison count table (mutant female chr2)
+temp_mt_f_e <- featureCounts(files="datafiles/mutant_female_2.bam",
+                       annot.ext="datafiles/Mus_musculus.GRCm38.98.gtf",
+                       isGTFAnnotationFile = TRUE, GTF.featureType="exon", GTF.attrType="gene_id", 
+                       isPairedEnd = TRUE)
+temp_mt_f_g <- featureCounts(files="datafiles/mutant_female_2.bam",
+                       annot.ext="datafiles/Mus_musculus.GRCm38.98.gtf",
+                       isGTFAnnotationFile = TRUE, GTF.featureType="gene", GTF.attrType="gene_id", 
+                       isPairedEnd = TRUE)
+
+result_e <- nonzero_counts(temp_mt_f_e$counts, results, "exons")
+result_g <- nonzero_counts(temp_mt_f_g$counts, results, "genes")
+result <- merge(result_e, result_g, by = "Gene_ID", all = TRUE)

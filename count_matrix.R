ad2d631a2fa7caf4398fe325030f6b590fa900c1
Justin Chee <justincy.chee@mail.utoronto.ca>
2019-12-20 02:38:13 -0500

    count_matrix.R 1.0

--- count_matrix.R
+++ count_matrix.R
@@ -1,35 +1,11 @@
 #!/usr/bin/env Rscript
 
 # script that produces count matrix from directory of bamfiles
+# arguments should be: 
+# 1. directory of bamfiles 2. gtf file 3. "combine" or "individ" (combine or use individual count matrices)
 
 library("Rsubread")
-
-# function to find genes with successful alignments
-nonzero_counts <- function(counts, cat) {
-  
-  # dataframe to store count data 
-  df <- data.frame(Gene_ID = character(),
-                   exons = integer(), 
-                   genes = integer(), 
-                   introns = integer())
-  
-  for (i in 1:length(counts)) {
-    if (counts[i] != 0) {
-      if (cat == "exons") {
-        df <- rbind(df, data.frame("Gene_ID" = rownames(counts)[i], "exons" = counts[i], 
-                                   "genes" = 0, "introns" = 0))
-      } else if (cat == "genes") {
-        df <- rbind(df, data.frame("Gene_ID" = rownames(counts)[i], "exons" = 0, 
-                                   "genes" = counts[i], "introns" = 0))
-      } else if (cat == "introns") {
-        df <- rbind(df, data.frame("Gene_ID" = rownames(counts)[i], "exons" = 0, 
-                                   "genes" = 0, "introns" = counts[i]))
-      }
-    }
-  }
-  
-  return(df)
-}
+library("dplyr")
 
 # function to create dataframe with exon, gene and intron data
 combine_counts <- function(e_count, g_count) {
@@ -63,8 +39,8 @@ args = commandArgs(trailingOnly = TRUE)
 
 if (length(args) == 0) {
   stop("One argument must be supplied (directory of bamfiles)", call.=FALSE)
-} else if (length(args) > 1) {
-  stop("No more than one argument must be supplied (directory of bamfiles)", call.=FALSE)
+} else if (length(args) > 3) {
+  stop("3 arguments must be supplied: 1. directory of bamfiles 2. gtf file 3. combine or individ", call.=FALSE)
 } else if (!dir.exists(args[1])) {
   stop("Invalid directory", call.False)
 }
@@ -72,4 +48,37 @@ if (length(args) == 0) {
 # list of bam files in given directory
 folder_files <- list.files(path = args[1], pattern = "\\.bam$")
 
-print(folder_files)
+# creating dataframe to hold combined counts
+if (args[3] == "combine") {
+  combined <- data.frame(Gene_ID = character(),
+                    exons = integer(), 
+                    genes = integer(), 
+                    introns = integer())
+}
+
+for (i in folder_files) {
+  
+  # variable holding exon counts
+  temp_e <- featureCounts(files = paste(args[1], "/", i, sep = ""), 
+                          annot.ext = args[2],
+                          isGTFAnnotationFile = TRUE, GTF.featureType="exon", GTF.attrType="gene_id", 
+                          isPairedEnd = TRUE)
+  # variable holding gene counts
+  temp_g <- featureCounts(files = paste(args[1], "/", i, sep = ""), 
+                          annot.ext = args[2],
+                          isGTFAnnotationFile = TRUE, GTF.featureType="gene", GTF.attrType="gene_id", 
+                          isPairedEnd = TRUE)
+  
+  # temp dataframe holding exon, gene and intron counts
+  temp_df <- combine_counts(temp_e$counts, temp_g$counts)
+  
+  if (args[3] == "individ") {  # write csv files for each bam file
+    write.csv(temp_df, file = paste(args[1], "/", tools::file_path_sans_ext(i), "_count.csv", sep = ""))
+  } else if (args[3] == "combine") {  # write combined csv file for all bam files
+    combined <- bind_rows(combined, temp_df) %>% group_by(Gene_ID) %>% summarise_all(sum)
+  }
+}
+
+if (args[3] == "combine") {
+  write.csv(combined, file = paste(args[1], "/", "combined_counts.csv", sep = ""))
+}
